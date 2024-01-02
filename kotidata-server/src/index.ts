0e2047f03fb8336b3821929d12173d6d1496c772
rheidi <hede.rajala@gmail.com>
2024-01-02 19:29:23 +0200

    Get server working

--- kotidata-server/src/index.ts
+++ kotidata-server/src/index.ts
@@ -1,15 +1,61 @@
 import express from "express"
 import cors from "cors"
+import axios from 'axios'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
+const LATEST_PRICES_ENDPOINT = 'https://api.porssisahko.net/v1/latest-prices.json'
+
+type Price = {
+  price: number
+  startDate: string
+  endDate: string
+}
+
+async function fetchLatestPriceData() {
+    const response = await axios.get(LATEST_PRICES_ENDPOINT)
+    return response.data
+}
+
+function getPriceForDate(date: Date, prices: Price[]) {
+    const matchingPriceEntry = prices.find(
+        (price) => new Date(price.startDate) <= date && new Date(price.endDate) > date
+    )
+
+    if (!matchingPriceEntry) {
+        throw 'Price for the requested date is missing'
+    }
+
+    return matchingPriceEntry.price
+}
+
 app.get("/api/notes", async (req, res) => {
-  res.json({ message: "success!" })
+    res.json({ message: "success!" })
 })
 
-app.listen(5000, () => {
-  console.log("server running on localhost:5000")
+app.get("/api/price-now", async (req, res) => {
+    try {
+        const { prices } = await fetchLatestPriceData() as { prices: Price[] }
+        const now = new Date()
+        const price = getPriceForDate(now, prices)
+        res.json({ message: `Hinta nyt (${now.toISOString()}): ${price} snt / kWh (sis. alv)` })
+    } catch (e) {
+        res.status(500).json({ error: `Hinnan haku epäonnistui, syy: ${e}` })
+    }
+})
+
+app.get("/api/prices", async (req, res) => {
+    try {
+        const { prices } = await fetchLatestPriceData() as { prices: Price[] }
+        res.send(prices)
+    } catch (e) {
+        res.status(500).json({ error: `Error message: ${e}` })
+    }
+})
+
+app.listen(5004, () => {
+    console.log("server running on localhost:5004")
 })
