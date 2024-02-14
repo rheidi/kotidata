3ed1a2a7bb9185e879a3b271a80fe70fcfbc6911
rheidi <hede.rajala@gmail.com>
2024-02-14 08:55:56 +0200

    Start working with graphics, show electricity prices in columns

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # kotidata
+
+## client
+
+client starts with `npm run dev` and runs in port 5173.
+
+## server
+
+server starts with `npm start`and runs in port 5004.
--- kotidata-client/src/components/Electricity.tsx
+++ kotidata-client/src/components/Electricity.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import 'charts.css'
 
 type Price = {
     price: number
@@ -43,17 +44,37 @@ const Electricity: React.FC = () => {
     return (
         <div>
             <h2>Current Price</h2>
-                <p>{priceNow()}</p>
-            <h2>Latest Prices</h2>
-            {prices.map((item, index) => (
-                <div key={index}>
-                    <p>From: {new Date(item.startDate).toLocaleString()}</p>
-                    <p>To: {new Date(item.endDate).toLocaleString()}</p>
-                    <p>Price: {item.price}</p>
-                </div>
-            ))}
+            <p>{priceNow()}</p>
+            <div id="price-chart">
+                <table className='charts-css column show-heading'>
+                    <caption>Latest Prices</caption>
+                        <tbody>{prices.map((item) => (
+                            <tr>
+                                
+                                <td style={{ '--size': '0.4' } as React.CSSProperties}> <span className="data"></span> {item.price}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                <ul className='charts-css legend'>
+
+                </ul>
+            </div>
         </div>
     );
 }
 
 export default Electricity
+
+{/*                     {prices.map((item, index) => (
+                        <div key={index}>
+                            <p>From: {new Date(item.startDate).toLocaleString()}</p>
+                            <p>To: {new Date(item.endDate).toLocaleString()}</p>
+                            <p>Price: {item.price}</p>
+                        </div>
+                    ))} 
+                <th scope="row"> From: {new Date(item.startDate).toLocaleString()}
+                            To: {new Date(item.endDate).toLocaleString()}
+                            </th>
+                
+                */}
